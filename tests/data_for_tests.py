3ab637669263b6d966f59055ed30f8cecf9c4c6d
SERHIISV <zeroproger@gmail.com>
2016-10-18 02:20:25 +0300

    Update data_for_tests.py

--- tests/data_for_tests.py
+++ tests/data_for_tests.py
@@ -1,4 +1,16 @@
-deck = '1 spades 1 hearts 1 clubs 1 diamonds 2 spades 2 hearts 2 clubs 2 diamonds 3 spades 3 hearts 3 clubs 3 diamonds 4 spades 4 hearts 4 clubs 4 diamonds 5 spades 5 hearts 5 clubs 5 diamonds 6 spades 6 hearts 6 clubs 6 diamonds 7 spades 7 hearts 7 clubs 7 diamonds 8 spades 8 hearts 8 clubs 8 diamonds 9 spades 9 hearts 9 clubs 9 diamonds 10 spades 10 hearts 10 clubs 10 diamonds 11 spades 11 hearts 11 clubs 11 diamonds 12 spades 12 hearts 12 clubs 12 diamonds 13 spades 13 hearts 13 clubs 13 diamonds'
+deck = '1 spades 1 hearts 1 clubs 1 diamonds\
+        2 spades 2 hearts 2 clubs 2 diamonds\
+        3 spades 3 hearts 3 clubs 3 diamonds\
+        4 spades 4 hearts 4 clubs 4 diamonds\
+        5 spades 5 hearts 5 clubs 5 diamonds\
+        6 spades 6 hearts 6 clubs 6 diamonds\
+        7 spades 7 hearts 7 clubs 7 diamonds\
+        8 spades 8 hearts 8 clubs 8 diamonds\
+        9 spades 9 hearts 9 clubs 9 diamonds\
+        10 spades 10 hearts 10 clubs 10 diamonds\
+        11 spades 11 hearts 11 clubs 11 diamonds\
+        12 spades 12 hearts 12 clubs 12 diamonds\
+        13 spades 13 hearts 13 clubs 13 diamonds'
 
 deck_more_then = [
     '6 spades',
